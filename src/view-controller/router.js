19b295fc0e26c4991612d9efab5358e32aef3edf
LindsayMuji <lmelissa.mp@gmail.com>
2022-02-11 18:21:05 -0500

    Pagina Home

---
+++ src/view-controller/router.js
@@ -0,0 +1,38 @@
+import { components } from '../view/components.js';
+
+const changeView = (route) => {
+    // vamos a asociar el nombre de la ruta con el componente
+    const mainContainer = document.getElementById('container');
+    // para que no agregue nada porque solo quiero que cambie
+
+    mainContainer.innerHTML = ' ';
+
+    switch (route) {
+        /*case '#/news': {
+          mainContainer.appendChild(components.News);
+          break;
+        }*/
+        case '': case '#/': case '#/home':
+            mainContainer.appendChild(components.Login());
+            break;
+
+        /*case '#/profileRegister': {
+          mainContainer.appendChild(components.Registro());
+          break;
+        }
+        case '#/profile': {
+          mainContainer.appendChild(components.Profile());
+          break;
+        }
+        case '#/sign-up': {
+          mainContainer.appendChild(components.SignUp());
+          registerUser();
+          break;
+        }
+        default:
+          mainContainer.appendChild(components.different());
+          break;
+      }*/
+    };
+};
+export { changeView };
---
+++ src/view/home.js
@@ -0,0 +1,33 @@
+export const logIn = ()=> {
+    const viewLogIn = `
+    <form id='formLogin' class='formLogin'>
+    <img class='turistik' src='../Images/home.jpg'>
+    <h2 class='tituloLogin'>TURISTI - K</h2>
+      <div class="containerSignIn"> 
+        <input type='text' placeholder='Ingrese su correo electrónico' id='emailLogin' class='emailLogin'>
+        <div class="eye">
+          <span class='iconEye'><i class="fas fa-eye-slash"></i></span>
+          <input type='password' placeholder='Ingrese su contraseña' id='pass' class='passLogin'>
+        </div>
+        <a class='forgetpass' id='forgetpass' href='#/'>¿Has olvidado tu contraseña?</a>  
+        <input type='submit' value='LogIn' id='save' class='save'>
+        <p id="textVerified"></p>
+        <div class='iconos_sesion'>
+          <img src='Images/google.png' id="btn-google" class="btn-google">
+          <img src='Images/facebook.png' id='btn-facebook' class= 'btn-facebook'>  
+        </div>
+        <p class="acountP">¿No tienes cuenta? <a href="#/signUp"> Regístrate aquí</a></p>
+      </div>
+    </form> `;
+
+const divElement = document.createElement('div');
+  divElement.setAttribute('id', 'contentLogin');
+  divElement.setAttribute('class', 'contentLogin');
+  divElement.innerHTML = viewLogIn;
+  /*const emailLogin = divElement.querySelector('#emailLogin');
+  const pass = divElement.querySelector('#pass');
+  const iconEye = divElement.querySelector('.iconEye');
+  const icon = divElement.querySelector('i');*/
+
+  return divElement;
+};
